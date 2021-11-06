63ec02e424836d989a2b501b2b0d1cf558bec626
pmabonfim <pmabonfim@gmail.com>
2021-11-06 18:37:28 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,6 +1,32 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+
+
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
 function DonutChart() {
+
+    // forma errada
+    let chartData : ChartData = { labels: [], series: [] };
+
+    // forma errada
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+          .then((response) => {
+              const data = response.data as SaleSum[];
+              const myLabels = data.map(x => x.sellerName);
+              const mySeries = data.map(x => x.sum);
+
+              chartData = { labels: myLabels, series: mySeries };
+
+              console.log(response.data);
+          });
+
     const options = {
         legend: {
             show: true
@@ -14,8 +40,8 @@ function DonutChart() {
 
     return (
         <Chart 
-            options={{ ...options, labels: mockData.labels}}
-            series={mockData.series}
+            options={{ ...options, labels: chartData.labels}}
+            series={chartData.series}
             type="donut"
             height="240"
         />
